03427895bb8267d9858274d90b5f4362cbd492a7
Carlos <hernandeasi7@gmail.com>
2022-10-28 22:52:37 -0300

    Added | Test config &  CalendarApi

---
+++ 09-calendar-app/tests/api/calendarApi.test.js
@@ -0,0 +1,15 @@
+import calendarApi from "../../src/api/calendarApi";
+
+describe("Pruebas en el CalendarApi", () => {
+  test("Debe tener la configuración por defecto", () => {
+    expect(calendarApi.defaults.baseURL).toBe(process.env.VITE_API_URL);
+  });
+
+  test("Debe tene el x-token en el header de todas las peticiones", async () => {
+    const token = "ABC-123-XYZ";
+    localStorage.setItem("token", token);
+    const res = await calendarApi.get("/auth");
+
+    expect(res.config.headers["x-token"]).toBe(token);
+  });
+});
