eb5618d8f045ffc42a97817204286a6862937bca
Carlos <hernandeasi7@gmail.com>
2022-11-02 21:06:50 -0300

    Unit Testing | FabDelete Tested

--- 09-calendar-app/tests/calendar/components/FabDelete.test.jsx
+++ 09-calendar-app/tests/calendar/components/FabDelete.test.jsx
@@ -1,15 +1,52 @@
-import { render, screen } from "@testing-library/react"
-import { Provider } from "react-redux";
-import { FabDelete } from "../../../src/calendar/components/FabDelete"
+import { fireEvent, render, screen } from "@testing-library/react";
+import { FabDelete } from "../../../src/calendar/components/FabDelete";
+import { useCalendarStore } from "../../../src/hooks/useCalendarStore";
 
-describe('Pruebas en <FabDelete />', () => {
+jest.mock("../../../src/hooks/useCalendarStore");
 
-  test('debe mostrar el componente correctamente', () => {
-    // render(
-      // <Provider>
-        {/* <FabDelete /> */}
-      {/* </Provider> */}
-    // );
-    // screen.debug()
-  })
-})
+describe("Pruebas en <FabDelete />", () => {
+  const mockStartDeletingEvent = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("debe mostrar el componente correctamente", () => {
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: false,
+    });
+
+    render(<FabDelete />);
+
+    const btn = screen.getByLabelText("btn-delete");
+    // console.log(btn.classList.toString());
+    expect(btn.classList).toContain("btn");
+    expect(btn.classList).toContain("btn-danger");
+    expect(btn.classList).toContain("fab-danger");
+    expect(btn.style.display).toBe("none");
+  });
+
+  test("debe mostrar el boton si hay un evento activo", () => {
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: true,
+    });
+
+    render(<FabDelete />);
+
+    const btn = screen.getByLabelText("btn-delete");
+    expect(btn.style.display).toBe("");
+  });
+
+  test("debe llamar startDeletingEvent si hay evento activo", () => {
+    useCalendarStore.mockReturnValue({
+      hasEventSelected: true,
+      startDeletingEvent: mockStartDeletingEvent,
+    });
+
+    render(<FabDelete />);
+
+    const btn = screen.getByLabelText("btn-delete");
+    fireEvent.click(btn);
+    expect(mockStartDeletingEvent).toHaveBeenCalled();
+  });
+});
